28e711d7988f92fa831694c20454e9b349195dcc
DarielEGM <darielegm@gmail.com>
2023-08-16 11:13:12 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const fecha = new Date();
+
+console.log(`Month:${fecha.getMonth()+1}  Day:${fecha.getDate()} Year:${fecha.getYear()+1900} 
+Hour:${fecha.getHours()} Minutes:${fecha.getMinutes()} Seconds:${fecha.getSeconds()}`); 
